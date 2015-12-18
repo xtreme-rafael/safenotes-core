db56a089bcd490c1674766f24bae26aa53bb34b4
Rafael Slobodian <rslobodian@pivotal.io>
2015-12-18 11:24:49 -0500

    RS - Added Notes struct and public methods to retrieve list of notes and add a note to the list.

---
+++ notes.go
@@ -0,0 +1,40 @@
+package safenotes_core
+
+import (
+	"github.com/pborman/uuid"
+	"time"
+	"encoding/json"
+)
+
+type Note struct {
+	Id string
+	Title string
+	Content string
+	Timestamp int64
+}
+
+var notes []Note
+
+func GetNotes() string {
+	json := serializeNotes()
+	return json
+}
+
+func serializeNotes() string {
+	json, _ := json.Marshal(notes)
+	return string(json)
+}
+
+func AddNote(title string, content string) {
+	note := makeNote(title, content)
+	notes = append(notes, note)
+}
+
+func makeNote(title string, content string) Note {
+	return Note{
+		Id: uuid.NewRandom().String(),
+		Title: title,
+		Content: content,
+		Timestamp: time.Now().Unix(),
+	}
+}
